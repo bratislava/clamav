200caf2bf632d2a15fa3d06acc5ebaceef95b197
Richi <richard.dvorsky@onlim.com>
2023-03-31 10:42:20 +0200

    removed freshclam from readiness

--- readiness.sh
+++ readiness.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-if freshclam | grep -q 'bytecode.cvd already up-to-date'; then
-  echo "freshclam running successfully"
-  if clamdscan virustest | grep -q 'Infected files: 1'; then
-    echo "Clamd running successfully"
-    exit 0
-  else
-    echo "Clamd not running"
-    exit 1
-  fi
+if clamdscan virustest | grep -q 'Infected files: 1'; then
+  echo "Clamd running successfully"
+  exit 0
 else
-  echo "freshclam not running"
+  echo "Clamd not running"
   exit 1
 fi
